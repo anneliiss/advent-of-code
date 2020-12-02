16f8122f8bab589ed032060d2bbb3df8af3b6cda
asaare <asaare@ttu.ee>
2020-12-02 20:02:06 +0200

    Day 2 part 2

--- 2020/2/solution.js
+++ 2020/2/solution.js
@@ -6,14 +6,24 @@ function solve(input, part) {
         let elements = row.split(": ");
         let rules = elements[0].split(" ");
         let limits = rules[0].split("-");
-        let lowerLim = parseInt(limits[0]);
-        let upperLim = parseInt(limits[1]);
-        let neededLetterRegExp =  new RegExp(rules[1], "g");
+        let firstLim = parseInt(limits[0]);
+        let secondLim = parseInt(limits[1]);
+        let neededLetter = rules[1];
         let password = elements[1];
-        var neededLettersInPw = password.match(neededLetterRegExp);
-        if (neededLettersInPw && neededLettersInPw.length >= lowerLim && neededLettersInPw.length <= upperLim) {
-            validPwCount++;
+        if (part == 1) {
+            let neededLetterRegExp =  new RegExp(neededLetter, "g");
+            var neededLettersInPw = password.match(neededLetterRegExp);
+            if (neededLettersInPw && neededLettersInPw.length >= firstLim && neededLettersInPw.length <= secondLim) {
+                validPwCount++;
+            }
+        } else if (part == 2) {
+            let charAtFirstLim = password.charAt(firstLim - 1);
+            let charAtSecondLim = password.charAt(secondLim - 1);
+            if ((charAtFirstLim != charAtSecondLim) && (charAtFirstLim == neededLetter || charAtSecondLim == neededLetter)) {
+                validPwCount++;
+            }
         }
+        
     });
     return validPwCount;
 
