9fe137b5a3948d0ca2e66f3ff4f355485d1078d8
asaare <asaare@ttu.ee>
2020-12-03 09:43:44 +0200

    Day 3 part 2

--- 2020/3/solution.js
+++ 2020/3/solution.js
@@ -1,22 +1,29 @@
 function solve(input, part) {
 
     let rows = Object.values(input);
+    let treeCount = countTrees(rows, 1, 3);
+    if (part == 1) {
+        return treeCount;
+    }
+    return treeCount * countTrees(rows, 1, 1) * countTrees(rows, 1, 5) * countTrees(rows, 1, 7) * countTrees(rows, 2, 1);  
+    
+}
+
+function countTrees(rows, rowIncr, colIncr) {
+    
     let rowLength = rows[0].length;
-    console.log(rowLength);
     let treeCount = 0;
     let col = 0;
-    for (let row = 1; row < rows.length; row++) {
-        col += 3;
-        if(rowLength <= col) {
+    for (let row = rowIncr; row < rows.length; row += rowIncr) {
+        col += colIncr;
+        if (rowLength <= col) {
             col = col - rowLength; 
         }
-        console.log(col);
         if (rows[row].charAt(col) == '#') {
             treeCount++;
         }
     }
     return treeCount;
-
 }
 
 module.exports = {solve};
