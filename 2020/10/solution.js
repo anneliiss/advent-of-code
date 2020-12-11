e4a103f268d3f3e1e002dd5c3801aaf95442f02b
asaare <asaare@ttu.ee>
2020-12-11 20:38:47 +0200

    Day 10 part 2

--- 2020/10/solution.js
+++ 2020/10/solution.js
@@ -5,27 +5,44 @@ function solve(input, part) {
     let deviceJoltage = adapters[0] + 3;
     adapters.unshift(deviceJoltage);
     adapters.push(0); //charging outlet joltage
+
     let oneJoltDiff = [];
-    let twoJoltDiff = [];
     let threeJoltDiff = [];
+
+    let groupLengthCounts = [0, 0, 0, 0]
+    let currentGroup = new Array();
+
     for (let i = 1; i < adapters.length; i++) {
         let currentAdapter = adapters[i];
         let difference = adapters[i - 1] - adapters[i];
         switch (difference) {
             case 1:
+                currentGroup.push(currentAdapter);
                 oneJoltDiff.push(currentAdapter);
-                break;
-            case 2:
-                twoJoltDiff.push(currentAdapter);
                 break;
             case 3:
                 threeJoltDiff.push(currentAdapter);
+                if (currentGroup.length > 0) {
+                    let len = currentGroup.length - 1;
+                    groupLengthCounts[len]++;
+                    currentGroup = new Array();
+                }
         }
         
     }
+
+    if (currentGroup.length > 0) {
+        let len = currentGroup.length - 1;
+        groupLengthCounts[len]++;
+    }
+    let result;
+
     if (part == 1) {
-        return oneJoltDiff.length * threeJoltDiff.length;
-    }   
+        result = oneJoltDiff.length * threeJoltDiff.length;
+    } else if (part == 2) {
+        result = Math.pow(2, groupLengthCounts[1]) * Math.pow(4, groupLengthCounts[2]) * Math.pow(7, groupLengthCounts[3]);
+    }
+    return result;
 }
 
 module.exports = {solve};
