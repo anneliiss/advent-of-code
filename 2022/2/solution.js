cae0ddd3acb3d021897e9aa74b18bafce0896178
Anneliis Saarend <anneliis@Anneliiss-MacBook-Pro.local>
2022-12-11 19:38:05 +0200

    2022 day 2 part 2

--- 2022/2/solution.js
+++ 2022/2/solution.js
@@ -1,26 +1,15 @@
-
-//rock - 1p
-//paper - 2p
-//scissors - 3p
-
 //A - rock
 //B - paper
 //C - scissors
 
-//X - rock?
-//Y - paper?
-//Z - scissors?
-
-//lose - 0p
-//draw - 3p
-//win - 6p
+//X - rock 1p - lose 0p
+//Y - paper 2p - draw 3p
+//Z - scissors 3p - win 6p
 
 //rock > scissors > paper > rock
 function solve(input, part) {
-    let opponentOptions = ['A', 'B', 'C'];
-    let myOptions = ['X', 'Y', 'Z'];
-
-    let perms =
+    let rounds = Object.values(input);
+    let permsForFirstPart =
         //opponent is rock
         {'A X': 4, //rock 1p + draw 3p
         'A Y': 8, //paper 2p + win 6p
@@ -33,11 +22,23 @@ function solve(input, part) {
         'C X': 7, //rock 1p + win 6p
         'C Y': 2, //paper 2p + lose 0p
         'C Z': 6}; //scissors 3p + draw 3p
-    let rounds = Object.values(input);
-    //console.log(rounds);
-    let results = rounds.map(round => perms[round]);
-    console.log(results);
-    if (part === 1) return results.reduce((a, c) => a + c, 0);
+    let resultsForFirstPart = rounds.map(round => permsForFirstPart[round]);
+    if (part === 1) return resultsForFirstPart.reduce((a, c) => a + c, 0);
+    let permsForSecondPart =
+        //opponent is rock
+        {'A X': 3, //scissors 3p + lose 0p
+        'A Y': 4, //rock 1p + draw 3p
+        'A Z': 8, //paper 2p + win 6p
+        //opponent is paper
+        'B X': 1, //rock 1p + lose 0p
+        'B Y': 5, //paper 2p + draw 3p
+        'B Z': 9, //scissors 3p + win 6p
+        //opponent is scissors
+        'C X': 2, //paper 2p + lose 0p
+        'C Y': 6, //scissors 3p + draw 3p
+        'C Z': 7}; //rock 1p + win 6p
+    let resultsForSecondPart = rounds.map(round => permsForSecondPart[round]);
+    if (part === 2) return resultsForSecondPart.reduce((a, c) => a + c, 0);
 
 }
 
