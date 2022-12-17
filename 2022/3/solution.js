73fa49d32febe7835dce7def1228de7e39c921e5
Anneliis Saarend <anneliis@Anneliiss-MacBook-Pro.local>
2022-12-17 16:28:49 +0200

    2022 day 3 part 2

--- 2022/3/solution.js
+++ 2022/3/solution.js
@@ -1,27 +1,41 @@
-//A given rucksack always has the same number of items in each of its two compartments, so the first half of the
-//characters represent items in the first compartment, while the second half of the characters represent items
-//in the second compartment.
-//Lowercase item types a through z have priorities 1 through 26.
-//Uppercase item types A through Z have priorities 27 through 52.
-//Find the item type that appears in both compartments of each rucksack.
-//What is the sum of the priorities of those item types?
 function solve(input, part) {
     let rucksacks = Object.values(input);
+    if (part === 1) return partOne(rucksacks);
+    if (part === 2) return partTwo(rucksacks);
+}
+
+function partOne(rucksacks) {
     let partOneAnswer = 0;
     rucksacks.forEach((rucksack) => {
         let itemsCount = rucksack.length;
         let firstCompartment = [...rucksack.substring(0, itemsCount / 2)];
         let secondCompartment = [...rucksack.substring(itemsCount / 2)];
         let repeatingItem = firstCompartment.find((item) => secondCompartment.includes(item));
-        let value = repeatingItem.charCodeAt(0);
-        //letter A has ASCII code 065, B 066 and so on until Z which is 090, then a has ASCII code 097, b 098 and so on.
-        //so in case the value is <=90, it is uppercase and 38 should be subtracted to get the value for this exercise.
-        //in case the value is >=97, it is lowercase and 96 should be subtracted
-        if (value <= 90) partOneAnswer += (value - 38)
-        else if (value >= 97) partOneAnswer += (value - 96)
+        partOneAnswer += calculatePriority(repeatingItem);
     })
-    if (part === 1) return partOneAnswer;
+    return partOneAnswer;
+}
 
+function partTwo(rucksacks) {
+    let partTwoAnswer = 0;
+    for (let i = 0; i < rucksacks.length - 2; i+=3) {
+        console.log(i);
+        let firstElf = [...rucksacks[i]];
+        let secondElf = [...rucksacks[i + 1]];
+        let thirdElf = [...rucksacks[i + 2]];
+        let badge = firstElf.find((item) => secondElf.includes(item) && thirdElf.includes(item));
+        partTwoAnswer += calculatePriority(badge);
+    }
+    return partTwoAnswer;
+}
+
+function calculatePriority(item) {
+    let value = item.charCodeAt(0);
+    //letter A has ASCII code 065, B 066 and so on until Z which is 090, then a has ASCII code 097, b 098 and so on.
+    //so in case the value is <=90, it is uppercase and 38 should be subtracted to get the value for this exercise.
+    //in case the value is >=97, it is lowercase and 96 should be subtracted
+    if (value <= 90) return (value - 38)
+    else if (value >= 97) return (value - 96)
 }
 
 module.exports = {solve};
